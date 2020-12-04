f693a99b79fd699fa715e6eb9b35da90d24bb6af
liujian3 <liujian3@gmail.com>
2020-12-04 11:28:32 +0800

    Update config.sh

--- node10_16_0_win64/config.sh
+++ node10_16_0_win64/config.sh
@@ -3,3 +3,4 @@ npm config set user-agent "npm/6.9.0 node/v10.16.0 win32 x64"
 npm config set registry http://$ADDR/repository/npm-proxy/
 yarn config set user-agent "yarn/1.22.10 npm/? node/v10.16.0 win32 x64"
 yarn config set registry http://$ADDR/repository/npm-proxy/
+ping 127.0.0.1
