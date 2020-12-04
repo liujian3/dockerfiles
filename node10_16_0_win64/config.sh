065ca93e547ee5e20b41ef272e6044f53abd8b60
liujian3 <liujian3@gmail.com>
2020-12-04 10:56:43 +0800

    Create config.sh

---
+++ node10_16_0_win64/config.sh
@@ -0,0 +1,4 @@
+npm config set user-agent "npm/6.9.0 node/v10.16.0 win32 x64"
+npm config set registry http://$1/repository/npm-proxy/
+yarn config set user-agent "yarn/1.22.10 npm/? node/v10.16.0 win32 x64"
+yarn config set registry http://$1/repository/npm-proxy/
